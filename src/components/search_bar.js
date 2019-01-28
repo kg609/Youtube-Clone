87c56763e27f791de2c21cba972f56a79a14b596
clear <kiwanegarner@Kiwanes-MacBook-Pro.local>
2019-01-28 00:05:01 -0500

    Reverted the web-dev-server back to version ^1.14.0

---
+++ src/components/search_bar.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {term: ""};
+  }
+
+  render(){
+    return (
+      <header>
+      <nav className="navbar navbar-expand-lg">
+        <ul className="navbar-nav">
+           <li className="nav-item active">
+           <a className="nav-link" href="#">Home</a>
+           </li>
+           <li className="nav-item">
+             <a className="nav-link" href="#">About</a>
+           </li>
+           <li className="nav-item">
+             <a className="nav-link" href="#">Contact</a>
+           </li>
+           <li className="nav-item">
+             <a className="nav-link" href="#">Link 4</a>
+           </li>
+        </ul>
+      </nav>
+        
+        <form className="input-group search-bar">
+          <input
+          className="form-control"
+          placeholder="What are you looking for..."
+          value={this.state.term}
+          onChange={(event) => this.onInputChange(event.target.value)} />
+          <span className="input-group-btn">
+            <button type="submit" className="btn btn-danger">Search</button>
+          </span>
+        </form>
+      </header>
+    );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+}
+
+export default SearchBar;
